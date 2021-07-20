815338f193fd2a5088d0e75a2771679842fdaae4
vinod mucha <vinodkyadav@gmail.com>
2021-07-20 11:09:47 +0000

    latest changes

---
+++ src/main/java/com/qf/app/App.java
@@ -0,0 +1,13 @@
+package com.qf.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
